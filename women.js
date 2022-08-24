90ad84a4f0ad403414cf3c5877e64fdb62d78f8f
RahulGeorge <rahulrsngs@gmail.com>
2022-08-24 20:37:38 +0530

    day2

---
+++ women.js
@@ -0,0 +1,27 @@
+
+    let imgarr=["https://www.jcrew.com/brand_creative/2022/202209-Sep/w_remix/v3/2022sept_0816_w_workremix_img7.jpg","https://www.jcrew.com/brand_creative/2022/202209-Sep/w_remix/v3/2022sept_0816_w_workremix_img5.jpg","https://www.jcrew.com/brand_creative/2022/202209-Sep/w_remix/v3/2022sept_0816_w_workremix_img12.jpg","https://www.jcrew.com/brand_creative/2022/202209-Sep/w_remix/v3/2022sept_0816_w_workremix_img15.jpg"]
+  
+let counter=0;
+let id;
+function start() {
+    let container=document.getElementById('sildeimages');
+
+    container.innerHTML=null;
+
+
+    let img=document.createElement("img");
+    id=setInterval(function(){
+        if(counter==imgarr.length){
+            counter=0;
+        }
+img.src=imgarr[counter];
+container.append(img);
+
+counter++; 
+    },3000)
+}
+
+
+function pause(){
+clearInterval(id);
+}
